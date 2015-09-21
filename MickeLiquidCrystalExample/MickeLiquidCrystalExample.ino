1bfcbb094bd2ad91b6b174314a977ee074d5a911
Micke <micke@home.com>
2015-09-21 14:42:51 +0200

    Add ino file

---
+++ MickeLiquidCrystalExample/MickeLiquidCrystalExample.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MickeLiquidCrystalExample.ino
+ Created:	9/21/2015 11:21:49 AM
+ Author:	Micke
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
